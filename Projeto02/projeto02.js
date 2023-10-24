d39bcc51d2c3806cd1d7c04527b34604545bcdfa
caroliv96 <121107004+caroliv96@users.noreply.github.com>
2023-10-24 19:44:24 -0300

    projetos 02 e 03

---
+++ Projeto02/projeto02.js
@@ -0,0 +1,8 @@
+var nome = 'Carolina';
+var sobrenome = ' De Oliveira';
+var idade = 27;
+
+console.log(`Meu nome é ${nome}${sobrenome}`);
+console.log(`Eu tenho ${idade} anos de idade`);
+        
+//solução do professor: console.log('Meu nome é ' + nome +' ' + sobrenome);
