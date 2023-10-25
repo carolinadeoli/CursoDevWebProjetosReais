8175c258c8311df875e84d95df52aae55910d788
caroliv96 <121107004+caroliv96@users.noreply.github.com>
2023-10-25 17:37:45 -0300

    projeto 07

---
+++ Projeto07/main.js
@@ -0,0 +1,13 @@
+
+let celsius = parseFloat(prompt('Digite a temperatura em Celsius:'));
+let f = (celsius * 1.8) +32;
+
+alert('A temperatura em Fahrenheit é ' + f.toFixed(2));
+
+//solução do professor:
+//let celsius =0;
+//let fahrenheit =0;
+// let celsius = parseFloat(prompt('Digite a temperatura em Celsius:'));
+// let fahrenheit = (celsius * 1.8) +32;
+// alert('A temperatura em Fahrenheit é ' + fahrenheit.toFixed(2));
+
