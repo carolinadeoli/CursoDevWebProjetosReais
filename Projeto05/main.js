6f3faab39aa4aa063490f83f41f6556d931010cf
caroliv96 <121107004+caroliv96@users.noreply.github.com>
2023-10-25 15:14:07 -0300

    projetos 5 e 6

---
+++ Projeto05/main.js
@@ -0,0 +1,5 @@
+let firstName = prompt('Qual é o seu nome?');
+let greeting ='Hello';
+document.getElementById('greeting').innerHTML = greeting + ' '+ firstName;
+
+//document.getElementById('greeting').innerHTML = 'Hello '+ firstName;
---
+++ Projeto06/main.js
@@ -0,0 +1,7 @@
+let friends = ['Alan', 'Mark', 'Julia','Bella'];
+friends[1] ='Anna' //replace
+friends[4] ='Maverick' //replace
+friends[5] = 1
+
+console.log(friends)
+console.log(friends[1])
